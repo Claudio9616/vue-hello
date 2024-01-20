0121c25aa9416170426fe1a533a932518af5fa84
Claudio9616 <clares9611@gmail.com>
2024-01-20 19:13:50 +0100

    completed

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('js', Vue)
+const {createApp} = Vue 
+const app = createApp({
+    name: 'Primo Vue',
+    data(){
+        return {
+            message: 'HELLO THERE',
+            pic: 'https://tse3.mm.bing.net/th?id=OIP.vMVvfd8CSp-m7Gl9wzm85gHaHa&pid=Api&P=0&h=180'
+        }
+    },
+    methods: {
+        myfunction() {
+            console.log('ciao Vue')   
+        }
+    }
+})
+app.mount('#root')
