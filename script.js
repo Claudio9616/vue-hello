6979b24e4cdc0a8ef3c5f8fee66378bc74153503
Claudio9616 <clares9611@gmail.com>
2024-01-21 19:44:09 +0100

    added super bonus

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const app = createApp({
     data(){
         return {
             message: 'HELLO THERE',
+            text: 'GENERAL KENOBI',
+            img: 'https://tse3.mm.bing.net/th?id=OIP.EwP5-UFEPz9wM7Ubkun2kAHaEo&pid=Api&P=0&h=180',
             pic: 'https://tse3.mm.bing.net/th?id=OIP.vMVvfd8CSp-m7Gl9wzm85gHaHa&pid=Api&P=0&h=180'
         }
     },
     methods: {
         myfunction() {
             console.log('ciao Vue')   
+            this.text
         }
     }
 })
